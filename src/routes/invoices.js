92c889e9bdb40f00763181f8708e3285cd62483c
allkaa <allkaa@yahoo.com>
2022-06-28 14:30:22 +0300

    react-router tutorial

---
+++ src/routes/invoices.js
@@ -0,0 +1,59 @@
+import {NavLink,  Outlet, useSearchParams, useLocation} from "react-router-dom";
+import { getInvoices } from "../data";
+
+export default function Invoices() {
+  let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+  let location = useLocation();
+  console.log(location);
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            let filter = event.target.value;
+            console.log(filter);
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get("filter");
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+            {invoice.name}
+            </NavLink>
+            )
+          )
+        }
+        <Outlet />
+      </nav>
+    </div>
+  );
+}
+//color: isActive ? "red" : "",
+//className={({ isActive }) => isActive ? "red" : "blue"}
