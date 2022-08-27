52df5d7bbfaf4805b86f433740ad599d1ed64f34
allkaa <allkaa@yahoo.com>
2022-08-27 11:49:02 +0300

    add build dir in workflow

---
+++ build/precache-manifest.68e0aac85e95e5f0b72279f04f1ffd6d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd05859a515dd7d7eb90",
+    "url": "/static/css/main.1ab04b18.chunk.css"
+  },
+  {
+    "revision": "dd05859a515dd7d7eb90",
+    "url": "/static/js/main.dd05859a.chunk.js"
+  },
+  {
+    "revision": "14c5c9ac4af67df9c71f",
+    "url": "/static/js/1.14c5c9ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8cafb62351188a22745871f7e7bb10cc",
+    "url": "/static/media/UNL_CAPASITY_HORIZONTAL_BLUE_LARGE.8cafb623.8cafb623.svg"
+  },
+  {
+    "revision": "b5a7c4e9745701a3ced35479087ccfcb",
+    "url": "/static/media/home-2-16.b5a7c4e9.ico"
+  },
+  {
+    "revision": "40ad6771d7c82f9bfd327fd1dcb53d43",
+    "url": "/static/media/photo5283283956105588195-1-348x215.40ad6771.jpg"
+  },
+  {
+    "revision": "0db9af097e6d18a51c9ca63764e8e280",
+    "url": "/static/media/SPEED_ICON.0db9af09.0db9af09.svg"
+  },
+  {
+    "revision": "469d03d8336b8b346f9b3497e3f33845",
+    "url": "/static/media/blitz.469d03d8.svg"
+  },
+  {
+    "revision": "c8f40c3a08e1c448aaf118f4afc7d1d0",
+    "url": "/static/media/super_loto.c8f40c3a.png"
+  },
+  {
+    "revision": "d1bc5086fac4ea322885446b5a57686a",
+    "url": "/static/media/keno.d1bc5086.png"
+  },
+  {
+    "revision": "5b485f047e755c1a764e2c7c9948d556",
+    "url": "/static/media/maxima.5b485f04.png"
+  },
+  {
+    "revision": "f7c97c084d12f18fe37dd04b925445f5",
+    "url": "/static/media/loto3.f7c97c08.png"
+  },
+  {
+    "revision": "13346f45d0139fc9cb355a55b4fa40bc",
+    "url": "/static/media/super_loto.13346f45.svg"
+  },
+  {
+    "revision": "32f0cd201a70fe396928a43988979e40",
+    "url": "/static/media/keno.32f0cd20.svg"
+  },
+  {
+    "revision": "b31023c32fa911ecdf361e558ce762dc",
+    "url": "/static/media/maxima.b31023c3.svg"
+  },
+  {
+    "revision": "a9d529344f42da21f2edffb38406f775",
+    "url": "/static/media/loto3.a9d52934.svg"
+  },
+  {
+    "revision": "c4b89f84120fbf06f7a903b728c83bc7",
+    "url": "/index.html"
+  }
+];
